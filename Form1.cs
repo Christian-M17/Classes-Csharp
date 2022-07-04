5456455eb340728c64bbb2a9c522ca20c8e53f1a
Christian Ottero <62633531+MasterM17@users.noreply.github.com>
2022-07-04 10:51:33 -0300

    Completado.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormMatematica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcessar_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            Matematica mat = new Matematica();
+            mat.Triplo(num1);
+            mat.Quadrado(num1);
+            mat.Soma(num1, num2);
+        }
+    }
+}
---
+++ Matematica.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormMatematica
+{
+    internal class Matematica
+    {
+        public void Triplo(int numero)
+        {
+            int resultado = numero * 3;
+            MessageBox.Show("O triplo de " + numero + " é: " + resultado);
+        }
+        public void Quadrado(int numero)
+        {
+            int resultado = numero * numero;
+            MessageBox.Show(numero + " Elevado ao quadrado é: " + resultado);
+        }
+        public void Soma(int numero1, int numero2)
+        {
+            int resultado = numero1 + numero2;
+            MessageBox.Show(numero1 + " somado a " + numero2 + " é: " + resultado);
+        }
+    }
+}
